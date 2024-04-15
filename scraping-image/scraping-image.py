98d59c02cf1f1d7991cf450c9c515294f922e418
hisao <hisao52321983@gmail.com>
2024-04-16 05:43:56 +0900

    複数画像取得、ダウンロード

--- scraping-image/scraping-image.py
+++ scraping-image/scraping-image.py
@@ -7,48 +7,65 @@ import urllib
 import os
 import requests
 from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
+# WebDriverのオプションを設定
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument("--no-sandbox")  # セキュリティ上の制限を回避
+options.add_argument("--lang=ja")     # 言語設定を日本語に
+
+# ChromeDriverを使用してWebDriverのインスタンスを作成
+driver = webdriver.Chrome(options=options)
+
+# URL開く
+driver.get("https://search.yahoo.co.jp/image")
+# 待機処理
+driver.implicitly_wait(10)
+#sleep(10)
+wait = WebDriverWait(driver=driver, timeout=10)
+ #検索窓 
+Word = "カフェ"
 # 最新のchromeドライバーをインストールして、インストール先のローカルパスを取得
-driver_path = ChromeDriverManager().install()
+#driver_path = ChromeDriverManager().install()
 # chromeドライバーのあるパスを指定して、起動
-driver = webdriver.Chrome(service=Service(executable_path=driver_path))
+#driver = webdriver.Chrome(service=Service(executable_path=driver_path))
 
-url = "https://search.yahoo.co.jp/image/search?p=%E3%82%AB%E3%83%95%E3%82%A7&aq=-1&oq=kafe&ai=202b4f32fa06f40-fe8462bc46f368-6fc715d7faa618-99aed221c534a8&at=s&ts=6428&sfp=1&ei=UTF-8&fr=sfp_as"
-driver.get(url=url)
+driver.find_element(By.CLASS_NAME, "SearchBox__searchInput").send_keys(Word)
+sleep(1)
+driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div[2]/form/div[1]/button").click()
+
+#待機処理
+wait.until(EC.presence_of_all_elements_located)
 
 # コピーしたXPathを使って画像のWeb要素を取得
 elements = driver.find_elements(By.TAG_NAME,"img")
-print(elements[0])
 
 # Web上の画像URLを取得
 img_urls=[]
 for img_url in elements:
     url_p=img_url.get_attribute("src")
     img_urls.append(url_p)
-    print(img_urls)
+    #print(img_urls)
 
-#取得データ保存
+#保存フォルダ作成、パス指定
 save_dir="download/"
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
 
-with urllib.request.urlopen(img_urls[0]) as rf:
-    img_data = rf.read()
+#バイナリデータ読み込み、保存
 a=1
-with open(save_dir + f"カフェ画像{a}.jpg","wb") as wf:
-    wf.write(img_data)
-
-"""
-    a=1
-for elem_url in img_urls:
+for img_url in img_urls:
     try:
-        r=requests.get(elem_url)
-        with open(save_dir + str("カフェ") + "画像"+str(a)+".jpg","wb") as fp:
-            fp.write(r.content0)
-        a += 1
+        with urllib.request.urlopen(img_url) as rf:
+            img_data = rf.read()
+        with open(save_dir + f"カフェ画像{a}.jpg","wb") as wf:
+            wf.write(img_data)
+        a=a+1
         sleep(1)
     except:
         pass
-"""
+
 # Webドライバーの終了
 driver.quit()
